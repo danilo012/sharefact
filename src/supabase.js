fc61ba458993c932b0fe4ee705a5971b62530d42
danilo012 <46241643+danilo012@users.noreply.github.com>
2023-04-24 08:49:43 +0800

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://hxdskxiekdqnhcpitkvu.supabase.co";
+const supabaseUrl = "https://dgxldofzgosnhclamzhi.supabase.co";
 
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRneGxkb2Z6Z29zbmhjbGFtemhpIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODE5MzU3MTYsImV4cCI6MTk5NzUxMTcxNn0.amHF8aMQ-N2zeadKjnsD1u4Gd3fP-ekEcHMBzeUtvbc";
